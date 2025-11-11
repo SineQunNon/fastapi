d394f829751dc1176d30487b5f797b9fdae743a9
SineQunNon <dleck28@gmail.com>
2025-11-11 15:41:33 +0900

    feat: 게시글 목록 조회 라우터 추가

--- routes/PostRouter.py
+++ routes/PostRouter.py
@@ -13,3 +13,7 @@ async def createPost(request: CreatePostRequest):
     # TODO
     return {"message": "게시글을 성공적으로 작성했습니다."}
 
+@router.get("")
+async def getPosts():
+    return {"message": "게시글 목록 조회를 완료했습니다."}
+
