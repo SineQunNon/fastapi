d87f4ad19288bde579f636e0dc76449977fabd3d
SineQunNon <dleck28@gmail.com>
2025-11-11 15:22:20 +0900

    feat: 회원가입 라우터 추가

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from routes import LoginRouter
+from routes import LoginRouter, UserRouter
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(LoginRouter.router)
+app.include_router(UserRouter.router)
