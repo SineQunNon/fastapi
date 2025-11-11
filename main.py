9b0244f0202cb6842640d487a3d788b409e40e74
SineQunNon <dleck28@gmail.com>
2025-11-11 16:14:23 +0900

    feat: 댓글 라우터 추가

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from routes import LoginRouter, UserRouter
+from routes import LoginRouter, UserRouter, PostRouter, CommentRouter
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(LoginRouter.router)
 app.include_router(UserRouter.router)
+app.include_router(PostRouter.router)
+app.include_router(CommentRouter.router)
