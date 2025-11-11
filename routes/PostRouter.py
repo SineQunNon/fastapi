f7fa71d368bd24b24c60f904dd2662f561814ab7
SineQunNon <dleck28@gmail.com>
2025-11-11 15:50:51 +0900

    feat: 게시글 수정 및 삭제 라우터 추가

--- routes/PostRouter.py
+++ routes/PostRouter.py
@@ -20,3 +20,17 @@ async def getPosts():
 @router.get("/{post_id}")
 async def getPost(post_id: int):
     return {"message" : "게시글 상세 조회를 성공했습니다."}
+
+
+class UpdatePostRequest(BaseModel):
+    title: str
+    content: str
+    postImage: str | None = None
+
+@router.put("/{post_id}")
+async def updatePost(post_id: int, request: UpdatePostRequest):
+    return {"message" : "게시글을 성공적으로 수정했습니다."}
+
+@router.delete("/{post_id}")
+async def deletePost(post_id: int):
+    return {"message" : "게시글을 성공적으로 삭제했습니다"}
