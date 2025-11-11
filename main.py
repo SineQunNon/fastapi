a7a323f11cd498606d33187c7c8d936776404686
SineQunNon <dleck28@gmail.com>
2025-11-11 12:40:33 +0900

    feat: LoginRouter 추가

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
+from routes import LoginRouter
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(LoginRouter.router)
