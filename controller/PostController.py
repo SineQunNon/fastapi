b0932bd90d826c1900ffd92695b2255ebedc4c57
SineQunNon <dleck28@gmail.com>
2025-11-12 16:27:05 +0900

    feat: 게시글 작성 구현

--- controller/PostController.py
+++ controller/PostController.py
@@ -1,10 +1,34 @@
-from pydantic import BaseModel
+from datetime import datetime
 
+from pydantic import BaseModel, field_validator
+
+postDatabase = {}
+post_id_count = 1
 
 class CreatePostRequest(BaseModel):
     title: str
     content: str
+    authorEmail: str
     postImage: str | None = None
+
+    @field_validator('title')
+    @classmethod
+    def valiateTitle(cls, value):
+        if not value or not value.strip():
+            raise ValueError("제목을 입력해주세요")
+
+        if len(value) > 26:
+            raise ValueError("제목은 26자 이하여야 합니다")
+
+        return value
+
+    @field_validator("content")
+    @classmethod
+    def validateContent(cls, value):
+        if not value or not value.strip():
+            raise ValueError("게시글 내용을 입력해주세요")
+
+        return value
 
 class UpdatePostRequest(BaseModel):
     title: str
@@ -14,8 +38,24 @@ class UpdatePostRequest(BaseModel):
 class PostController:
     @staticmethod
     def createPost(request: CreatePostRequest):
-        # TODO
-        return {"message": "게시글을 성공적으로 작성했습니다"}
+        global post_id_count
+        post_id = post_id_count
+        post_id_count += 1
+
+        postDatabase[post_id] = {
+            "title": request.title,
+            "content": request.content,
+            "authorEmail": request.authorEmail,
+            "postImage": request.postImage,
+            "createdAt": datetime.now().isoformat(),
+            "likes": 0,
+            "views": 0,
+        }
+
+        return {
+            "message": "게시글을 성공적으로 작성했습니다",
+            "post_id": post_id
+        }
 
     @staticmethod
     def getPosts():
