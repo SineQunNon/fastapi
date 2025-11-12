2d4f5481515b21e55a3461a8fb82a8e3cf645ef4
SineQunNon <dleck28@gmail.com>
2025-11-12 13:45:43 +0900

    feat: 로그인 route controller 분리
    - LoginController 추가
    - login 구체적 기능을 LoginController에게 위임

--- routes/LoginRouter.py
+++ routes/LoginRouter.py
@@ -1,14 +1,8 @@
 from fastapi import APIRouter
-from pydantic import BaseModel, EmailStr
+from controller.LoginController import LoginController, LoginRequest
 
 router = APIRouter(prefix="/login", tags=["login"])
 
-class LoginRequest(BaseModel):
-    email: EmailStr
-    password: str
-
 @router.post("/")
 async def login(request: LoginRequest):
-    # TODO
-    return {"message": "로그인에 성공했습니다"}
-
+    return LoginController.login(request)
