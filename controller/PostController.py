78c74ba3763a623840b51d8e2d873546c4d2628b
SineQunNon <dleck28@gmail.com>
2025-11-12 16:51:32 +0900

    feat: 게시글 상세 목록 조회 구현

--- controller/PostController.py
+++ controller/PostController.py
@@ -2,8 +2,30 @@ from datetime import datetime
 
 from pydantic import BaseModel, field_validator
 
-postDatabase = {}
-post_id_count = 1
+postDatabase = {
+    1 : {
+        "post_id": 1,
+        "title": "그것이 알고 싶다",
+        "content": "그것이 알고 싶다의 피디 땡땡땡입니다....",
+        "authorEmail": "dleck28@gmail.com",
+        "postImage": None,
+        "createdAt": datetime.now().isoformat(),
+        "likes": 0,
+        "views": 0,
+    },
+    2 : {
+        "post_id": 2,
+        "title": "그것이 알기 싫다",
+        "content": "그것이 알기 싫다의 피디 땡땡땡입니다....",
+        "authorEmail": "dleck28@gmail.com",
+        "postImage": None,
+        "createdAt": datetime.now().isoformat(),
+        "likes": 0,
+        "views": 0,
+    },
+
+}
+post_id_count = 3
 
 class CreatePostRequest(BaseModel):
     title: str
@@ -43,6 +65,7 @@ class PostController:
         post_id_count += 1
 
         postDatabase[post_id] = {
+            "post_id": post_id,
             "title": request.title,
             "content": request.content,
             "authorEmail": request.authorEmail,
@@ -59,8 +82,23 @@ class PostController:
 
     @staticmethod
     def getPosts():
-        # TODO
-        return {"message": "게시글 목록을 성공적으로 조회했습니다"}
+        posts = []
+
+        for post in postDatabase.values():
+            posts.append({
+                "title" : post["title"],
+                "content": post["content"],
+                "authorEmail": post["authorEmail"],
+                "postImage": post["postImage"],
+                "createdAt": post["createdAt"],
+                "likes": post["likes"],
+                "views": post["views"],
+            })
+        return {
+            "message": "게시글 목록을 성공적으로 조회했습니다",
+            "posts": posts,
+            "total" : len(posts)
+        }
 
     @staticmethod
     def getPost(post_id: int):
