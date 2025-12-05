578e21a56c647ef73743b29157385f0f4b50336f
SineQunNon <dleck28@gmail.com>
2025-12-05 13:52:15 +0900

    refactor: 사용자 관련 기능 리팩토링

--- controllers/LoginController.py
+++ controllers/LoginController.py
@@ -1,24 +1,26 @@
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
-from controllers.UserController import userDatabase
+from models.User import User
 from schemas.UserSchema import LoginRequest
 
 class LoginController:
     @staticmethod
-    def login(request: LoginRequest):
-        if request.email not in userDatabase:
+    def login(request: LoginRequest, db: Session):
+        user = db.query(User).filter(User.email == request.email).first()
+
+        if not user:
             raise HTTPException(
                 status_code=401,
                 detail="아이디 또는 비밀번호를 확인해주세요."
             )
-
-        user = userDatabase[request.email]
 
         if not user.verify_password(request.password):
             raise HTTPException(
                 status_code=401,
                 detail="아이디 또는 비밀번호를 확인해주세요."
             )
+
         return {
             "message": "로그인에 성공했습니다",
             "user": {
--- routes/LoginRouter.py
+++ routes/LoginRouter.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
-from controllers.LoginController import LoginController, LoginRequest
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from controllers.LoginController import LoginController
+from schemas.UserSchema import LoginRequest
+from db.database import get_db
 
 router = APIRouter(prefix="/login", tags=["login"])
 
 @router.post("/")
-async def login(request: LoginRequest):
-    return LoginController.login(request)
+async def login(request: LoginRequest, db: Session = Depends(get_db)):
+    return LoginController.login(request, db)
